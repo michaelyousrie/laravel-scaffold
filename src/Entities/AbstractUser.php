2ec3a9badc9e4c15abdf222154789dd59d53a4ce
Michael <michaelyousrie@gmail.com>
2020-09-01 09:51:12 +0200

    update the abstract user class to include the base user data.

--- src/Entities/AbstractUser.php
+++ src/Entities/AbstractUser.php
@@ -2,10 +2,40 @@
 
 namespace LaravelScaffold\Entities;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 abstract class AbstractUser extends Authenticatable
 {
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     public function group()
     {
         return $this->belongsTo(UserGroup::class, 'user_group_id');

d2fce7aaf4a347be6b00927abed9731706fb69fe
Michael <michaelyousrie@gmail.com>
2020-10-06 02:17:05 +0200

    make user group and user group permission factoried.

--- src/Models/UserGroup.php
+++ src/Models/UserGroup.php
@@ -3,9 +3,12 @@
 namespace LaravelScaffold\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserGroup extends Model
 {
+    use HasFactory;
+
     public function permissions()
     {
         return $this->hasMany(UserGroupPermission::class);
--- src/Models/UserGroupPermission.php
+++ src/Models/UserGroupPermission.php
@@ -3,8 +3,9 @@
 namespace LaravelScaffold\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserGroupPermission extends Model
 {
-    //
+    use HasFactory;
 }

fb71ef407e020ff9e7743cf284fe0e02ef652f43
Michael <michaelyousrie@gmail.com>
2020-10-06 05:37:58 +0200

    another update.

--- src/Traits/ImageFunctionality.php
+++ src/Traits/ImageFunctionality.php
@@ -10,7 +10,7 @@ trait ImageFunctionality
 {
     public function addImage(UploadedFile $file)
     {
-        $file = FileUploader::upload($file, storage_path("images"));
+        $file = FileUploader::upload($file, "images");
 
         $image = new Image([
             'path'      => $file
